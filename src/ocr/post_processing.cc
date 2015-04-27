d6ab6a6e695e82dc5fb822c9d971f6d60eebdac3
meriembendris <m.bendris@gmail.com>
2015-04-27 10:35:06 +0200

    rien

--- src/ocr/post_processing.cc
+++ src/ocr/post_processing.cc
@@ -6,8 +6,7 @@ using namespace std;
 // Martin Ettl, 2012-10-05
  
 
-size_t uiLevenshteinDistance(const std::string &s1, const std::string &s2)
-{
+size_t uiLevenshteinDistance(const std::string &s1, const std::string &s2){
   const size_t m(s1.size());
   const size_t n(s2.size());
  
